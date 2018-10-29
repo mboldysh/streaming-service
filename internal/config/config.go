c4e48cb3a8fe5fefd223be03c7bc19b43a26799d
mboldysh <maksym.boldysh@gmail.com>
2018-10-29 15:21:59 +0200

    Remove AWS_REGION check

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"errors"
 	"flag"
 	"time"
 
@@ -87,9 +86,9 @@ func createS3Client(cfg aws.Config, dev bool) (*s3.S3, error) {
 			return nil, err
 		}
 
-		if cfg.Region == "" {
-			return nil, errors.New("Set AWS_REGION env variable")
-		}
+		// if cfg.Region == "" {
+		// 	return nil, errors.New("Set AWS_REGION env variable")
+		// }
 
 		s3Client = s3.New(cfg)
 	}
