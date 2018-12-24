1360e097bf3c200dad6048a3fb4be583f9e513b6
mboldysh <maksym.boldysh@gmail.com>
2018-12-24 02:43:25 +0200

    Change AllowedHeaders policy

--- cmd/streaming-service/main.go
+++ cmd/streaming-service/main.go
@@ -36,7 +36,7 @@ func main() {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		MaxAge:           300,
